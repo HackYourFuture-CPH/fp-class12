6eea3bc5516ad4ba3e46c9b1fa1ac0bfa0e821ba
Zaki Wasik <zakiwa@gmail.com>
2020-05-10 04:13:24 +0200

    fix: this file was not in use

--- src/server/api/routes/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const modulesRouter = require('./modules.router');
-
-router.use('/modules', modulesRouter);
-
-module.exports = router;
