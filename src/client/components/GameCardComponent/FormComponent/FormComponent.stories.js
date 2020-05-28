0d6913887ab7e09072e644e4dc7903a0d721f813
Luciana Caruso <lucianac2005@gmail.com>
2020-05-28 14:11:54 +0200

    Game Card Issue #19: Added Form Component

---
+++ src/client/components/GameCardComponent/FormComponent/FormComponent.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import FormComponent from './FormComponent';
+
+export default { title: 'Form Component ' };
+
+export const Component = () => (
+  <FormComponent inputLabel="Team name" value="Enter name" />
+);
