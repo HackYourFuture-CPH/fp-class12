1faa501664eb30c06b907a1203700c89504f14fe
Luciana Caruso <lucianac2005@gmail.com>
2020-05-28 15:49:49 +0200

    Delete duplicated commit for Button Component

--- src/client/components/FormComponent/ButtonComponent/ButtonComponent.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ButtonComponent from './ButtonComponent';
-
-export default { title: 'Button Component' };
-
-export const Component = () => <ButtonComponent buttonTitle="Trial button" />;
