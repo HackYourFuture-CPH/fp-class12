f2ead5302986831de79ba386f1aa702ac7ced56a
Mekdes <yesmek@yahoo.com>
2020-05-29 11:30:10 +0200

    Title component

---
+++ src/client/components/Title/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Title.css';
+
+const Title = ({ title }) => {
+  return <div className="title">{title}</div>;
+};
+
+export default Title;
---
+++ src/client/components/Title/Title.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Title from './Title';
+
+export default { title: 'Game Title' };
+
+export const GameTitle = () => <Title title="Title Game" />;
