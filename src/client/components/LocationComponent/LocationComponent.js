27b784e3b3f382de6c3552b41598bade1b919f70
Mekdes <yesmek@yahoo.com>
2020-05-30 23:22:48 +0200

    updated Game description style based on updated images

--- src/client/components/LocationComponent/LocationComponent.js
+++ src/client/components/LocationComponent/LocationComponent.js
@@ -4,8 +4,11 @@ class LocationComponent extends React.Component {
   state = { address: '' };
 
   componentDidMount() {
+    const latitude = 55.661869;
+    const longitude = 12.540742;
+
     fetch(
-      'https://api.opencagedata.com/geocode/v1/json?q=55.661869+12.540742&key=216a941e5d7e4b5dbd4c778c87486d35',
+      `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=216a941e5d7e4b5dbd4c778c87486d35`,
     )
       .then((response) => response.json())
       .then((data) => {
