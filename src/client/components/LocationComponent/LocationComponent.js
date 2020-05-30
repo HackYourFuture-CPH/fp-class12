5cb9efc2565862d862da32a291fb3984bd692335
Mekdes <yesmek@yahoo.com>
2020-05-30 12:40:16 +0200

    made changed on naming

--- src/client/components/LocationComponent/LocationComponent.js
+++ src/client/components/LocationComponent/LocationComponent.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 class LocationComponent extends React.Component {
-  state = { adress: '' };
+  state = { address: '' };
 
   componentDidMount() {
     fetch(
       'https://api.opencagedata.com/geocode/v1/json?q=55.661869+12.540742&key=216a941e5d7e4b5dbd4c778c87486d35',
     )
-      .then((respoonse) => respoonse.json())
+      .then((response) => response.json())
       .then((data) => {
-        this.setState({ adress: data.results[0].formatted });
+        this.setState({ address: data.results[0].formatted });
       });
   }
 
   render() {
-    return <p>{this.state.adress}</p>;
+    return <p>{this.state.address}</p>;
   }
 }
 
