564b19cd9362b153cbb0a1017d14305d452336b7
benhmaid <ghofranebh90@gmail.com>
2020-05-30 23:32:24 +0200

    Edited Timer function

--- src/client/components/Timer/Timer.js
+++ src/client/components/Timer/Timer.js
@@ -1,38 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import './Timer.styles.css';
-
-// prepends `0` to single digit number
-const getTimeWithDoubleDigits = (numberString) =>
-  `${numberString}`.padStart(2, '0');
-
-function formatTime(timeLeftInSecond) {
-  const secondsPrMinute = 160;
-  // Deriving minutes from total seconds and rounding it
-  const minutes = Math.floor(timeLeftInSecond / secondsPrMinute);
-  const formattedMinutes = getTimeWithDoubleDigits(minutes);
-
-  //  Deriving remaining seconds from total seconds
-  const seconds = timeLeftInSecond - secondsPrMinute * minutes;
-  const formattedSeconds = getTimeWithDoubleDigits(seconds);
-
-  return `${formattedMinutes}:${formattedSeconds}`;
-}
 
 const Timer = ({ countdown }) => {
   const [counter, setCounter] = useState(countdown);
 
   useEffect(() => {
-    const timer =
-      counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+    const timer = counter && setTimeout(() => setCounter(counter - 1), 1000);
     return () => clearInterval(timer);
   }, [counter]);
+
   return (
     <div className="timer-content">
-      <span>{formatTime(counter)}</span>
+      <span>{moment.unix(counter).format('mm:ss')}</span>
     </div>
   );
 };
+
 Timer.propTypes = {
   countdown: PropTypes.number.isRequired,
 };
