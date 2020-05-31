8369e3c48fed3a1ee7b9dbcc7b19b37ea0a923ca
Luciana Caruso <lucianac2005@gmail.com>
2020-05-31 12:52:44 +0200

    Erased unnecesary nested structure

--- src/client/components/GameCardComponent/FormComponent/FormComponent.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import FormComponent from './FormComponent';
-
-export default { title: 'Form Component ' };
-
-export const Component = () => (
-  <FormComponent inputLabel="Team name" value="Enter name" />
-);
