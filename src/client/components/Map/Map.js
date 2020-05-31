6615f89feb01222886e03ba91f7d061b1ef49b52
Mekdes <yesmek@yahoo.com>
2020-06-01 00:02:11 +0200

    updated leaflet class component

--- src/client/components/Map/Map.js
+++ src/client/components/Map/Map.js
@@ -1,37 +1,25 @@
-import React from 'react';
-import { Map, TileLayer, Marker } from 'react-leaflet';
-
+import React, { useEffect } from 'react';
 import '.../../../node_modules/leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-class Mymap extends React.Component {
-  state = {
-    lat: 55.661869,
-    lng: 12.540742,
-    zoom: 13,
-  };
+function Mymap({ latitude, longitude }) {
+  useEffect(() => {
+    L.map('map', {
+      center: [latitude, longitude],
+      zoom: 13,
+      layers: [
+        L.tileLayer(
+          'https://dev.{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
+          {
+            attribution:
+              '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          },
+        ),
+      ],
+    });
+  });
 
-  render() {
-    const coordinates = [this.state.lat, this.state.lng];
-    return (
-      <Map
-        center={coordinates}
-        zoom={this.state.zoom}
-        style={{
-          height: '300px',
-          width: '100%',
-          borderTopRightRadius: '15px',
-          borderTopLeftRadius: '15px',
-        }}
-      >
-        <TileLayer
-          attribution='<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://dev.{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
-        />
-
-        <Marker position={coordinates} />
-      </Map>
-    );
-  }
+  return <div id="map" />;
 }
 
 export default Mymap;
--- src/client/components/Map/Map.stories.js
+++ src/client/components/Map/Map.stories.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Mymap from './Map';
 
+import './Map.css';
+
 export default { title: 'Map' };
 
-export const Map = () => <Mymap />;
+export const Map = () => <Mymap latitude="55.661869" longitude="12.540742" />;
