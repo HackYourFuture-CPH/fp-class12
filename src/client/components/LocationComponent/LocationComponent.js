4d8a9fa52f82ec6cc4e3e8880aed13b2f0ab1299
Mekdes <yesmek@yahoo.com>
2020-06-01 00:41:10 +0200

    updated class location class component

--- src/client/components/LocationComponent/LocationComponent.js
+++ src/client/components/LocationComponent/LocationComponent.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class LocationComponent extends React.Component {
-  state = { address: '' };
+function LocationComponent() {
+  const latitude = 55.661869;
+  const longitude = 12.540742;
+  const [location, setLocation] = useState();
 
-  componentDidMount() {
-    const latitude = 55.661869;
-    const longitude = 12.540742;
+  fetch(
+    `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=216a941e5d7e4b5dbd4c778c87486d35`,
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      setLocation(data.results[0].formatted);
+    });
 
-    fetch(
-      `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=216a941e5d7e4b5dbd4c778c87486d35`,
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ address: data.results[0].formatted });
-      });
-  }
-
-  render() {
-    return <p>{this.state.address}</p>;
-  }
+  return <p>{location}</p>;
 }
 
 export default LocationComponent;
