d205984090d9de21a51fb8c534674943dc25bbd0
Mekdes <yesmek@yahoo.com>
2020-06-01 00:44:37 +0200

    added props validation to title component

--- src/client/components/Title/Title.js
+++ src/client/components/Title/Title.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import './Title.css';
 
 const Title = ({ title }) => {
   return <div className="title">{title}</div>;
+};
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default Title;
