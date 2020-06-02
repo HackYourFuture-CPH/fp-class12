c7fdb59cd813ae10828d3d36dfe69510f38531da
Swathi Sankararaman <swathisankarraman@gmail.com>
2020-06-02 23:01:36 +0200

    Knex_created answer_choices table

---
+++ src/server/migrations/20200602225007_answer_choices.js
@@ -0,0 +1,14 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('answer_choices', (table) => {
+    table.increments('id');
+    table.string('answer_text');
+    table.integer('fk_question_id');
+    table.boolean('answer_correct');
+    table.timestamp('created_at').notNullable();
+    table.timestamp('updated_at').notNullable();
+    table.timestamp('deleted_at');
+  });
+};
+exports.down = function (knex) {
+  return knex.schema.dropTable('answer_choices');
+};
