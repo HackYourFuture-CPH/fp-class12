9cec1543d1fd1daaa0d3b0221aa10e2ca27d0afe
myshuker <myshuker@gmail.com>
2020-06-03 23:25:11 +0200

    Game_instances and Actual_answers #42

---
+++ src/server/migrations/20200602233442_create_game_instances_table.js
@@ -0,0 +1,15 @@
+// TODO: This is a sample migration for demonstration. Remove before adding real ones.
+
+exports.up = function (knex) {
+  return knex.schema.createTable('game_instances', (table) => {
+    table.increments();
+    table.integer('score');
+    table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
+    table.datetime('updated_at').defaultTo(knex.fn.now()).notNullable();
+    table.datetime('deleted_at');
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('game_instances');
+};
