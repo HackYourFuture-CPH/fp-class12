858d03175b9db4c90bc7e6c38cb46206059aaee9
amerdahabi <aamerdahabi@gmail.com>
2020-06-03 19:55:40 +0200

    fixed images paths and improve css

--- src/client/components/LeaderboardList/users.js
+++ src/client/components/LeaderboardList/users.js
@@ -1,7 +1,7 @@
-import image1 from '../../assets/images/avatar1-ce.png';
-import image2 from '../../assets/images/avatar2-ce.png';
-import image3 from '../../assets/images/avatar3-ce.png';
-import image4 from '../../assets/images/avatar4-ce.png';
+import image1 from '../../assets/images/avatars/black-avatar-circle.png';
+import image2 from '../../assets/images/avatars/girl-avatar-circle.png';
+import image3 from '../../assets/images/avatars/guy-avatar-circle.png';
+import image4 from '../../assets/images/avatars/man-avatar-circle.png';
 
 export const users = [
   {
