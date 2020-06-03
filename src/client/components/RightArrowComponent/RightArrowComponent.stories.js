f199343e5b5e0182adf80509771f02a8b1a40f12
Swathi Sankararaman <swathisankarraman@gmail.com>
2020-06-03 14:17:16 +0200

    Implemented right arrow component

---
+++ src/client/components/RightArrowComponent/RightArrowComponent.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import RightArrowComponent from './RightArrowComponent';
+
+export default { title: 'Right Arrow Component' };
+
+export const RightArrow = () => <RightArrowComponent />;
