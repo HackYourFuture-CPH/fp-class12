14bb1b72b3825bc89124d7bed838a1925ad99a29
Luciana Caruso <lucianac2005@gmail.com>
2020-06-06 16:14:35 +0200

    Corrected tile (instead of title)

--- src/server/migrations/20200602211147_themes.js
+++ src/server/migrations/20200602211147_themes.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('themes', (table) => {
     table.increments('id');
-    table.string('title');
+    table.string('tile');
     table.timestamp('created_at').notNullable();
     table.timestamp('updated_at').notNullable();
     table.timestamp('deleted_at');
