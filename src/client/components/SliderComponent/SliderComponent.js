d97e2d2bc4c0e1f805c8f8146e73f38774fcb243
Swathi Sankararaman <swathisankarraman@gmail.com>
2020-06-07 22:49:42 +0200

    Implemented feedback to place the arrows on either side of the image

--- src/client/components/SliderComponent/SliderComponent.js
+++ src/client/components/SliderComponent/SliderComponent.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ArrowsComponent from '../ArrowsComponent/ArrowsComponent';
 import SlideshowImageComponent from '../SlideshowImageComponent/SlideshowImageComponent';
 import SliderDotsComponent from '../SliderDotsComponent/SliderDotsComponent';
 import avatarImages from '../../assets/data/ArrayOfImages';
 import './SliderComponent.styles.css';
 
-class Slider extends React.Component {
+function Slider() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const totalImages = avatarImages.length;
+
+  function slideImage(direction) {
+    setCurrentImageIndex(direction(currentImageIndex));
+  }
+
+  const left = (activeImageIndex) => {
+    return activeImageIndex < 1 ? totalImages - 1 : activeImageIndex - 1;
+  };
+
+  const right = (activeImageIndex) => {
+    return activeImageIndex === totalImages - 1 ? 0 : activeImageIndex + 1;
+  };
+
+  const handleDotsClick = (event, index) => {
+    setCurrentImageIndex(index);
+  };
+
+  return (
+    <div className="slideshow_container">
+      <div className="arrow_component">
+        <ArrowsComponent
+          clickLeftArrow={() => slideImage(left)}
+          clickRightArrow={() => slideImage(right)}
+        />
+      </div>
+      <div className="avatarImages_component">
+        <SlideshowImageComponent currentImageIndex={currentImageIndex} />
+      </div>
+      <div className="magicdots_component">
+        <SliderDotsComponent
+          clickDots={handleDotsClick}
+          noOfDots={totalImages}
+          activeImageIndex={currentImageIndex}
+        />
+      </div>
+    </div>
+  );
+}
+/* class Slider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -70,6 +111,6 @@ class Slider extends React.Component {
       </main>
     );
   }
-}
+} */
 
 export default Slider;
