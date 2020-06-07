9eab5c28f75f774b5c93dc3b4ec0ebb414ff53c0
Zaki Wasik <zakiwa@gmail.com>
2020-06-07 16:34:23 +0200

    Prettier fixes

--- src/client/components/ResultMessageModalWindow/ResultTask.component.js
+++ src/client/components/ResultMessageModalWindow/ResultTask.component.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 
 export default function ResultTask({ correctAnswer }) {
   if (correctAnswer > 3) {
-    return <h3 className="success_task" >Congratulations!</h3>
+    return <h3 className="success_task">Congratulations!</h3>;
   }
-    return <h3 className="failed_task">Incorrect Answered!</h3>
+  return <h3 className="failed_task">Incorrect Answered!</h3>;
 }
 
 ResultTask.propTypes = {
