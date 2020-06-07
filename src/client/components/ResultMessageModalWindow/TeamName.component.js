ec26875ec3f64e3b31be8817c8f25ffcaede4141
amerdahabi <aamerdahabi@gmail.com>
2020-06-07 16:18:30 +0200

    fixed format pretier

--- src/client/components/ResultMessageModalWindow/TeamName.component.js
+++ src/client/components/ResultMessageModalWindow/TeamName.component.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import './ResultMessageModalWindow.styles.css';
 
 export default function TeamName({ title }) {
-  return <h3 className="team_name-component"> Team {title} </h3>
-  }
+  return <h3 className="team_name-component"> Team {title} </h3>;
+}
 
 TeamName.propTypes = {
   title: PropTypes.string.isRequired,
