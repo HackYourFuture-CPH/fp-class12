37d9b1a25e2258b7b430e66ae25c7ed6348c6e83
Luciana Caruso <lucianac2005@gmail.com>
2020-06-09 12:56:55 +0200

    Added FK according to issue. Removed external FK

--- src/server/migrations/20200602213728_game_factory.js
+++ src/server/migrations/20200602213728_game_factory.js
@@ -1,9 +1,10 @@
 exports.up = function (knex) {
   return knex.schema.createTable('game_factory', (table) => {
     table.increments('id');
-    table.integer('fk_map_id');
-    table.integer('fk_theme_id').notNullable();
-    table.integer('fk_game_user_id').notNullable();
+    table.integer('fk_map_id').unsigned();
+    table.foreign('fk_map_id').references('id').inTable('maps');
+    table.integer('fk_theme_id').unsigned();
+    table.foreign('fk_theme_id').references('id').inTable('themes');
     table.integer('max_players');
     table.timestamp('created_at').notNullable();
     table.timestamp('updated_at').notNullable();
