d84906a787f8756c6ead9fd43180b70a7c1720e4
Mohammad alshoker <myshuker@gmail.com>
2020-06-10 20:10:17 +0200

    change timestamp to datetime in actual answers table

--- src/server/migrations/20200608220954_actual_answers.js
+++ src/server/migrations/20200608220954_actual_answers.js
@@ -9,9 +9,9 @@ exports.up = function (knex) {
     table.foreign('fk_question_id').references('id').inTable('questions');
     table.integer('fk_answer_id').unsigned();
     table.foreign('fk_answer_id').references('id').inTable('answer_choices');
-    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
-    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
-    table.timestamp('deleted_at').defaultTo(knex.fn.now());
+    table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
+    table.datetime('updated_at').defaultTo(knex.fn.now()).notNullable();
+    table.datetime('deleted_at');
   });
 };
 
