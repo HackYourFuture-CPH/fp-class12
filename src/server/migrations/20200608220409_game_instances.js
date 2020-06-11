08624de3b393d898ba35748fa3f03cc66020205a
Mohammad alshoker <myshuker@gmail.com>
2020-06-11 22:47:28 +0200

    delete fk from game instances table that are related to others tables

--- src/server/migrations/20200608220409_game_instances.js
+++ src/server/migrations/20200608220409_game_instances.js
@@ -4,10 +4,6 @@ exports.up = function (knex) {
   return knex.schema.createTable('game_instances', (table) => {
     table.increments('id');
     table.integer('score');
-    table.integer('fk_user_id').unsigned();
-    table.foreign('fk_user_id').references('id').inTable('users');
-    table.integer('fk_game_id').unsigned();
-    table.foreign('fk_game_id').references('id').inTable('game_factory');
     table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
     table.datetime('updated_at').defaultTo(knex.fn.now()).notNullable();
     table.datetime('deleted_at');
