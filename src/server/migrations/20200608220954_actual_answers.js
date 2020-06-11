26e53c859e48b58e216384bf471b29355f72098d
Mohammad alshoker <myshuker@gmail.com>
2020-06-11 22:48:07 +0200

    delete fk from actual answers table that are related to others tables

--- src/server/migrations/20200608220954_actual_answers.js
+++ src/server/migrations/20200608220954_actual_answers.js
@@ -5,10 +5,6 @@ exports.up = function (knex) {
     table.increments('id');
     table.integer('fk_game_user_id').unsigned();
     table.foreign('fk_game_user_id').references('id').inTable('game_instances');
-    table.integer('fk_question_id').unsigned();
-    table.foreign('fk_question_id').references('id').inTable('questions');
-    table.integer('fk_answer_id').unsigned();
-    table.foreign('fk_answer_id').references('id').inTable('answer_choices');
     table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
     table.datetime('updated_at').defaultTo(knex.fn.now()).notNullable();
     table.datetime('deleted_at');
