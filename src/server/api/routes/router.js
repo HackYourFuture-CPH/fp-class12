a4a2b85a6d5d597c92bc9871e9004aab9326b793
Luciana Caruso <lucianac2005@gmail.com>
2020-06-12 22:06:56 +0200

    Added endpoint for Game instances

--- src/server/api/routes/router.js
+++ src/server/api/routes/router.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 const modulesRouter = require('./modules.router');
 
+const gameInstancesRouter = require('./gameInstances.router');
+
 router.use('/modules', modulesRouter);
+router.use('/gameInstances', gameInstancesRouter);
 
 module.exports = router;
