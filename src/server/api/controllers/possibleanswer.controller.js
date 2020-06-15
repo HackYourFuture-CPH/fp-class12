64d93e1451a6e5f1f4badcdbcf82d330de814aad
priyankapandey13 <55982058+priyankapandey13@users.noreply.github.com>
2020-06-15 23:42:56 +0200

    Update possibleanswer.controller.js

--- src/server/api/controllers/possibleanswer.controller.js
+++ src/server/api/controllers/possibleanswer.controller.js
@@ -16,7 +16,7 @@ const getPossibleanswerById = async (id) => {
   try {
     const modules = await knex('answer_choices')
       .select('fk_question_id', 'answer_text')
-      .where('fk_question_id', '=', { id });
+      .where('fk_question_id', id);
 
     if (modules.length === 0) {
       throw new Error(`incorrect entry with the id of ${id}`, 404);
