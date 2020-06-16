87d6d2ab47335503f4c417c2dd68d4b152091704
Mohammad alshoker <myshuker@gmail.com>
2020-06-16 23:08:20 +0200

    change id to fk_game_id in the controller

--- src/server/api/controllers/game_Instance.controller.js
+++ src/server/api/controllers/game_Instance.controller.js
@@ -2,8 +2,7 @@ const knex = require('../../config/db');
 
 const createGameInstance = async (body) => {
   await knex('game_instances').insert({
-    id: body.id,
-    max_players: body.max_players,
+    fk_game_id: body.fk_game_id,
 
   });
 
