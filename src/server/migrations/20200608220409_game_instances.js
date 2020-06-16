e36cb6c5c7ec729144b73ead9f2c3d46900297e7
John <john.yazji@gmail.com>
2020-06-17 00:12:55 +0200

    modified datetime to timestamp and add notNullable() to fk-02

--- src/server/migrations/20200608220409_game_instances.js
+++ src/server/migrations/20200608220409_game_instances.js
@@ -1,15 +1,15 @@
 exports.up = function (knex) {
   return knex.schema.createTable('game_instances', (table) => {
     table.increments('id');
-    table.integer('fk_game_scores_id');
+    table.integer('fk_game_scores_id').unsigned().notNullable();
     table.foreign('fk_game_scores_id').references('id').inTable('game_scores');
-    table.integer('fk_user_id');
+    table.integer('fk_user_id').unsigned().notNullable();
     table.foreign('fk_user_id').references('id').inTable('users');
-    table.integer('fk_game_id');
+    table.integer('fk_game_id').unsigned().notNullable();
     table.foreign('fk_game_id').references('id').inTable('game_factory');
-    table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
-    table.datetime('updated_at').defaultTo(knex.fn.now()).notNullable();
-    table.datetime('deleted_at');
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('deleted_at');
   });
 };
 
--- src/server/migrations/20200616213133_game_scores.js
+++ src/server/migrations/20200616213133_game_scores.js
@@ -1,13 +1,9 @@
-
 exports.up = function (knex) {
   return knex.schema.createTable('game_scores', (table) => {
     table.increments('id');
-    table.integer('fk_game_scores_id').unsigned().notNullable();
-    table.foreign('fk_game_scores_id').references('id').inTable('game_score');
+    table.integer('score');
     table.integer('fk_user_id').unsigned().notNullable();
-    table.foreign('fk_user_id').references('id').inTable('user');
-    table.integer('fk_game_id').unsigned().notNullable();
-    table.foreign('fk_game_id').references('id').inTable('game_factory');
+    table.foreign('fk_user_id').references('id').inTable('users');
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
     table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
     table.timestamp('deleted_at');
