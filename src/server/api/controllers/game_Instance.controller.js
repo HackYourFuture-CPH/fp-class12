8533f4fea9ba23701e141a44c3969bd541c74b19
Mohammad alshoker <myshuker@gmail.com>
2020-06-16 22:31:04 +0200

    delete update and create fields from the controller

--- src/server/api/controllers/game_Instance.controller.js
+++ src/server/api/controllers/game_Instance.controller.js
@@ -1,12 +1,10 @@
 const knex = require('../../config/db');
-const moment = require('moment-timezone');
 
 const createGameInstance = async (body) => {
   await knex('game_instances').insert({
     id: body.id,
     max_players: body.max_players,
-    created_at: moment(body.created_at).format(),
-    updated_at: moment(body.updated_at).format(),
+
   });
 
   return {
