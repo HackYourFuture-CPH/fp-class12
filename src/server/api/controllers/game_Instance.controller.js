5ced781930a88a509b83709a93f39d35b598f571
Mohammad alshoker <myshuker@gmail.com>
2020-06-17 20:00:29 +0200

    add timer mode to game_Instance controller

--- src/server/api/controllers/game_Instance.controller.js
+++ src/server/api/controllers/game_Instance.controller.js
@@ -3,7 +3,7 @@ const knex = require('../../config/db');
 const createGameInstance = async (body) => {
   await knex('game_instances').insert({
     fk_game_id: body.fk_game_id,
-
+    timer_mode: body.time_mode,
   });
 
   return {
