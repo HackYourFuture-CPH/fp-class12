6e7338291fe79d12cc493a305ac70ce1fe0b6d41
krithi-kondana <krithi1789@gmail.com>
2020-06-17 19:30:39 +0200

    added fk in game_instance table

--- src/server/migrations/20200608220409_game_instances.js
+++ src/server/migrations/20200608220409_game_instances.js
@@ -3,7 +3,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable('game_instances', (table) => {
     table.increments('id');
-    table.integer('score');
+    table.integer('fk_game_scores_id').unsigned();
+    table.integer('fk_user_id').unsigned();
+    table.integer('fk_game_id').unsigned();
+    table.foreign('fk_game_scores_id').references('id').inTable('game_scores');
+    table.foreign('fk_user_id').references('id').inTable('users');
+    table.foreign('fk_game_id').references('id').inTable('game_factory');
     table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
     table.datetime('updated_at').defaultTo(knex.fn.now()).notNullable();
     table.datetime('deleted_at');
