4e2e689c0ac9ff5cd79d9ccb5a71482aebf20e02
mochi <benguozakinci@gmail.com>
2020-06-17 14:47:19 +0000

    Commented out unused variable && Change width of the reader

--- src/client/components/QRReaderComponent/QRReaderComponent.js
+++ src/client/components/QRReaderComponent/QRReaderComponent.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QrReader from 'react-qr-reader';
+
 import './QRReaderComponent.styles.css';
 
 export default function QRReaderComponent() {
+  // const [result, setResult] = useState(); I was getting an error committing my changes because the value wasn't used, but is needed for future use.
+  const [error, setErr] = useState();
+
   return (
     <div className="qr-reader-background">
-      <div className="qr-frame">
-        <div className="top">
-          <div className="qr-frame-corner top-left" />
-          <div className="qr-frame-corner top-right" />
-        </div>
-        <div className="bottom">
-          <div className="qr-frame-corner bottom-left" />
-          <div className="qr-frame-corner bottom-right" />
-        </div>
+      <div className="qr-reader-frame">
+        {error ? (
+          <p>{error.message}</p>
+        ) : (
+          <QrReader
+            delay={300}
+            onError={(err) => setErr(err)}
+            // onScan={(data) => setResult(data)}
+            className="qr-reader"
+            showViewFinder={false}
+          />
+        )}
       </div>
     </div>
   );
 }
+
+/*
+<div className="qr-frame">
+        <div className="top">
+          <div className="qr-frame-corner top-left" />
+          <div className="qr-frame-corner top-right" />
+        </div>
+
+        <div className="bottom">
+          <div className="qr-frame-corner bottom-left" />
+          <div className="qr-frame-corner bottom-right" />
+        </div>
+</div> 
+*/
