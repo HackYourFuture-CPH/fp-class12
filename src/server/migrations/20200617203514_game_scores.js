3b3c9d6bc39b2265ed6f36b56784248710f00636
krithi-kondana <krithi1789@gmail.com>
2020-06-17 21:48:11 +0200

    added notNullable to fk

--- src/server/migrations/20200617203514_game_scores.js
+++ src/server/migrations/20200617203514_game_scores.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('game_scores', (table) => {
     table.increments('id');
     table.integer('score');
-    table.integer('fk_user_id').unsigned();
+    table.integer('fk_user_id').unsigned().notNullable();
     table.foreign('fk_user_id').references('id').inTable('users');
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
     table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
