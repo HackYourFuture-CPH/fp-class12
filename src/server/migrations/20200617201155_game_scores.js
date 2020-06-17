d42d3807d1579c6095693e1f003273f64e9be8ae
krithi-kondana <krithi1789@gmail.com>
2020-06-17 20:17:16 +0200

    adding foreign keys to game_instances and game_scores table

--- src/server/migrations/20200617201155_game_scores.js
+++ src/server/migrations/20200617201155_game_scores.js
@@ -1,12 +1,9 @@
 exports.up = function (knex) {
-  return knex.schema.createTable('game_instances', (table) => {
+  return knex.schema.createTable('game_scores', (table) => {
     table.increments('id');
-    table.integer('fk_game_scores_id').unsigned();
+    table.integer('score');
     table.integer('fk_user_id').unsigned();
-    table.integer('fk_game_id').unsigned();
-    table.foreign('fk_game_scores_id').references('id').inTable('game_scores');
     table.foreign('fk_user_id').references('id').inTable('users');
-    table.foreign('fk_game_id').references('id').inTable('game_factory');
     table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
     table.datetime('updated_at').defaultTo(knex.fn.now()).notNullable();
     table.datetime('deleted_at');
@@ -14,5 +11,5 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable('game_instances');
+  return knex.schema.dropTable('game_scores');
 };
