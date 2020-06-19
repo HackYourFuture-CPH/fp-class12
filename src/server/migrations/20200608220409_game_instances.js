11451e957dfc989ac599f92d8dad002327ef57bf
krithi-kondana <krithi1789@gmail.com>
2020-06-19 21:55:51 +0200

    added fk and notNullable to gameinstances table

--- src/server/migrations/20200608220409_game_instances.js
+++ src/server/migrations/20200608220409_game_instances.js
@@ -1,12 +1,15 @@
-// TODO: This is a sample migration for demonstration. Remove before adding real ones.
-
 exports.up = function (knex) {
   return knex.schema.createTable('game_instances', (table) => {
     table.increments('id');
-    table.integer('score');
-    table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
-    table.datetime('updated_at').defaultTo(knex.fn.now()).notNullable();
-    table.datetime('deleted_at');
+    table.integer('fk_game_scores_id').unsigned().notNullable();
+    table.integer('fk_user_id').unsigned().notNullable();
+    table.integer('fk_game_id').unsigned().notNullable();
+    table.foreign('fk_game_scores_id').references('id').inTable('game_scores');
+    table.foreign('fk_user_id').references('id').inTable('users');
+    table.foreign('fk_game_id').references('id').inTable('game_factory');
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('deleted_at');
   });
 };
 
