f0cdf2afd17707341e98a18a58fbf6e774b352d3
Swathi Sankararaman <swathisankarraman@gmail.com>
2020-06-19 06:17:06 +0200

    added drop FK to fix error in github

--- src/server/migrations/20200617125636_add_fk_avatar_id.js
+++ src/server/migrations/20200617125636_add_fk_avatar_id.js
@@ -7,6 +7,7 @@ exports.up = function (knex) {
 
 exports.down = function (knex) {
   return knex.schema.table('users', function (table) {
+    table.dropForeign('fk_avatar_id');
     table.dropColumn('fk_avatar_id');
   });
 };
