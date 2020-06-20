8be14e95e2525274fb499c80c2523b4295bfb574
myshuker <myshuker@gmail.com>
2020-06-20 14:39:58 +0200

    change type of time

--- src/server/migrations/20200619230308_game_timer_field.js
+++ src/server/migrations/20200619230308_game_timer_field.js
@@ -1,7 +1,6 @@
 exports.up = function (knex) {
     return knex.schema.table('game_scores', (table) => {
-      table.time('game_timer');
-      
+      table.time('game_timer');      
     });
   };
   
