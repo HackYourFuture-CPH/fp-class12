9253695ab55e63b41b076a248796145a3f61c9ad
priyankapandey13 <55982058+priyankapandey13@users.noreply.github.com>
2020-06-20 17:23:33 +0200

    Update router.js

--- src/server/api/routes/router.js
+++ src/server/api/routes/router.js
@@ -9,6 +9,7 @@ router.use('/modules', modulesRouter);
 router.use('/answer_choices', possibleanswerRouter);
 
 const questionsRouter = require('./questions.router');
+
 router.use('/questions', questionsRouter);
 
 module.exports = router;
