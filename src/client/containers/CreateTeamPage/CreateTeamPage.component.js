d77ebeceb2a0b139ed98e191f9c4ae53a95ccd63
mochi <benguozakinci@gmail.com>
2020-06-21 13:02:59 +0000

    Add initial structure of page

--- src/client/containers/CreateTeamPage/CreateTeamPage.component.js
+++ src/client/containers/CreateTeamPage/CreateTeamPage.component.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import HeaderTitle from '../../components/Title/Title';
+import Footer from '../../components/FooterComponent/FooterComponent'
 
 export const CreateTeamPage = () => (
-  <HeaderTitle title="This is the Create Team Page" />
+  <section className="container">
+    <HeaderTitle title="This is the Create Team Page" />
+    <main></main>
+    <Footer />
+  </ section>
 );
