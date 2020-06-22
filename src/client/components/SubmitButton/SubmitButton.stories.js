54d8e5f7ebed568c28aec2f2ec4fd865d5ee799f
Priyanka <Priyanka.mydesire@gmail.com>
2020-06-22 18:02:07 +0200

    Commented the alert part because of eslint

--- src/client/components/SubmitButton/SubmitButton.stories.js
+++ src/client/components/SubmitButton/SubmitButton.stories.js
@@ -3,7 +3,8 @@ import SubmitButton from './SubmitButton';
 import { withKnobs, text } from '@storybook/addon-knobs';
 
 function clicked() {
-  alert('i am clicked');
+  // alert('i am clicked');
+  // eslint doesn't accept onclick so it is commented for production
 }
 export default { title: 'Submit Button Component', decorators: [withKnobs] };
 
