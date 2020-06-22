6e974058f95794ed4aba9fb2f117e2d4ce4ce061
Priyanka <Priyanka.mydesire@gmail.com>
2020-06-22 17:31:31 +0200

    Onclick event for submit button

--- src/client/components/SubmitButton/SubmitButton.stories.js
+++ src/client/components/SubmitButton/SubmitButton.stories.js
@@ -2,9 +2,12 @@ import React from 'react';
 import SubmitButton from './SubmitButton';
 import { withKnobs, text } from '@storybook/addon-knobs';
 
+function clicked() {
+  alert('i am clicked');
+}
 export default { title: 'Submit Button Component', decorators: [withKnobs] };
 
 export const Submitbutton = () => {
   const ButtonTitle = text('title', 'Trial button');
-  return <SubmitButton ButtonTitle={ButtonTitle} />;
+  return <SubmitButton ButtonTitle={ButtonTitle} onClick={clicked} />;
 };
