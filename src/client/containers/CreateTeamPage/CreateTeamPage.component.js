fb319034da5e6e26435d3f0dcfb98670e3694512
mochi <benguozakinci@gmail.com>
2020-06-22 17:08:48 +0000

    Add components and images

--- src/client/containers/CreateTeamPage/CreateTeamPage.component.js
+++ src/client/containers/CreateTeamPage/CreateTeamPage.component.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderTitle from '../../components/Title/Title';
-import Footer from '../../components/FooterComponent/FooterComponent'
+import Footer from '../../components/FooterComponent/FooterComponent';
+import SingleInputFormComponent from '../../components/SingleInputFormComponent/SingleInputFormComponent';
+
+import anonAvatarImage from '../../assets/images/avatars/anonimos-avatar.png';
+import addIcon from '../../assets/images/icons/plus-icon.svg';
 
 export const CreateTeamPage = () => (
   <section className="container">
     <HeaderTitle title="This is the Create Team Page" />
-    <main></main>
+    <main>
+      <p>Make sure to fill out your name or the name of the team and avatar you are playing on before we start.</p>
+      <img src={anonAvatarImage} alt="silhouette of a person pointing out user is not yet chosen an avatar yet"/>
+      <Link to="/pick-avatar-page">
+        <img src={addIcon} alt="button to pick avatar" />
+      </Link>
+      <SingleInputFormComponent inputLabel="Team name" value="Enter name" ButtonTitle="Create Team"/>
+    </main>
     <Footer />
-  </ section>
+  </section>
 );
