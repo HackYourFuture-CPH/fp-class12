38e01fad4476fb5039ea778aeda7e323b97e64c4
Luciana Caruso <lucianac2005@gmail.com>
2020-06-23 10:46:33 +0200

    Added field code, created_at and updated_at to show in the response

--- src/server/api/controllers/gameInstances.controller.js
+++ src/server/api/controllers/gameInstances.controller.js
@@ -4,7 +4,14 @@ const getGameInstances = async () => {
   try {
     return await knex('game_instances')
       .join('users', 'users.id', 'game_instances.fk_user_id')
-      .select('game_instances.id', 'users.name', 'game_instances.score')
+      .select(
+        'game_instances.id',
+        'users.name',
+        'game_instances.score',
+        'game_instances.code',
+        'game_instances.created_at',
+        'game_instances.updated_at',
+      )
       .where('finished', '=', false);
   } catch (error) {
     return error.message;
