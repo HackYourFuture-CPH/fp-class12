71491a9369f41a2169c57a75313854bfca38ed79
John <john.yazji@gmail.com>
2020-06-24 20:36:08 +0200

    code changed beased on finished field added

--- src/server/api/controllers/getAllUsers.controller.js
+++ src/server/api/controllers/getAllUsers.controller.js
@@ -2,10 +2,12 @@ const knex = require('../../config/db');
 
 const getAllUsers = async () => {
   try {
-    return await knex('game_scores')
-      .select('game_scores.fk_user_id', 'game_scores.score')
-      .orderBy('game_scores.score', 'desc')
-      .limit(2);
+    return await knex('users')
+      .join('game_instances', 'users.id', '=', 'game_instances.fk_user_id')
+      .join('game_scores', 'users.id', '=', 'game_scores.fk_user_id')
+      .select('users.name', 'game_scores.score')
+      .where ('game_instances.finished', '=', '1')
+      .orderBy('game_scores.score', 'desc');
   } catch (error) {
     return error.message;
   }
