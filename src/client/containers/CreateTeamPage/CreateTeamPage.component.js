8d13576e0c4012b7baf58719af4f0e1a4a6408ef
mochi <benguozakinci@gmail.com>
2020-06-24 16:29:09 +0000

    Add component for add avatar page

--- src/client/containers/CreateTeamPage/CreateTeamPage.component.js
+++ src/client/containers/CreateTeamPage/CreateTeamPage.component.js
@@ -3,9 +3,7 @@ import { Link } from 'react-router-dom';
 import HeaderTitle from '../../components/Title/Title';
 import Footer from '../../components/FooterComponent/FooterComponent';
 import SingleInputFormComponent from '../../components/SingleInputFormComponent/SingleInputFormComponent';
-
-import anonAvatarImage from '../../assets/images/avatars/anonimos-avatar.png';
-import addIcon from '../../assets/images/icons/plus-icon.svg';
+import AddAvatarComponent from '../../components/AddAvatarComponent/AddAvatarComponent'
 
 export const CreateTeamPage = () => (
   <div id="root">
@@ -16,12 +14,8 @@ export const CreateTeamPage = () => (
           Make sure to fill out your name or the name of the team and avatar you
           are playing on before we start.
         </p>
-        <img
-          src={anonAvatarImage}
-          alt="silhouette of a person pointing out user is not yet chosen an avatar yet"
-        />
         <Link to="/pick-avatar-page">
-          <img src={addIcon} alt="button to pick avatar" />
+          <AddAvatarComponent />
         </Link>
         <SingleInputFormComponent
           inputLabel="Team name"
