78d0975a281b1daabc67cfe8d9083c61544191b7
Mekdes <yesmek@yahoo.com>
2020-06-25 11:03:26 +0200

    adding first demo

--- src/client/containers/ShareGamePage/ShareGamePage.component.js
+++ src/client/containers/ShareGamePage/ShareGamePage.component.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import '../../App.css';
+import SubmitButton from '../../components/SubmitButton/SubmitButton';
 import HeaderTitle from '../../components/Title/Title';
+import Footer from '../../components/FooterComponent/FooterComponent';
+import QRcode from '../../components/QRcode/QRcode';
+import { Link } from 'react-router-dom';
 
+const level = 'L';
 export const ShareGamePage = () => (
-  <HeaderTitle title="This is the Share Game Page" />
+  <section className="home bodycontainer">
+    <HeaderTitle title="Scan QR-Code" />
+    <div className="bodywrapper">
+      <p>share this code to the players to share this code</p>
+      <div className="homelogocontainer">
+        <div className="QRcode">
+          <QRcode
+            value="random code"
+            fgColor="black"
+            bgColor="white"
+            size={250}
+            level={level}
+            includeMargin={false}
+          />
+        </div>
+      </div>
+      <Link to="/">
+        <SubmitButton ButtonTitle="Back to Game" />
+      </Link>
+    </div>
+    <Footer title="Test title" />
+  </section>
 );
