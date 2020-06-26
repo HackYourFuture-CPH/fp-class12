6a6256217eb93b654a150a7db461554a743e8f00
amerdahabi <aamerdahabi@gmail.com>
2020-06-26 14:48:35 +0200

    solve conflict

--- src/server/api/routes/router.js
+++ src/server/api/routes/router.js
@@ -23,13 +23,11 @@ const createUserProfilesRouter = require('./create-user-profiles.router');
 const createUserRolesRouter = require('./create-user-roles.router');
 
 router.use('/answer_choices', possibleanswerRouter);
-router.use('/modules', modulesRouter);
 router.use('/createUserProfiles', createUserProfilesRouter);
 router.use('/createUserRoles', createUserRolesRouter);
 router.use('/next_question', nextQuestion);
 router.use('/questions', questionsRouter);
 router.use('/allUsers', allUsersRouter);
 router.use('/qr', qrRouter);
-router.use('/gameInstances', gameInstancesRouter);
 
 module.exports = router;
