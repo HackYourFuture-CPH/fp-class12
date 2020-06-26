d9bbc6e44587f288a3202d87c43422ed5c590681
Swathi Sankararaman <swathisankarraman@gmail.com>
2020-06-26 12:55:06 +0200

    oined game_instance table as per amer's feedback

--- src/server/api/controllers/getQuestionById.controller.js
+++ src/server/api/controllers/getQuestionById.controller.js
@@ -4,6 +4,11 @@ const getQuestionById = async (gameFactoryId, questionId) => {
   try {
     const questions = await knex('questions')
       .join('game_factory', 'questions.fk_game_factory_id', 'game_factory.id')
+      .join(
+        'game_instances',
+        'game_factory.id',
+        'game_instances.fk_game_factory_id',
+      )
       .select('questions.question')
       .where('game_factory.id', gameFactoryId)
       .where('questions.id', questionId);
