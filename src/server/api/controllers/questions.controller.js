a3c92257f84a5f0ca92042182fa2b99128988987
Mekdes <yesmek@yahoo.com>
2020-06-28 12:31:02 +0200

    commiting changes on questions controller based on the changes on the develop

--- src/server/api/controllers/questions.controller.js
+++ src/server/api/controllers/questions.controller.js
@@ -3,7 +3,12 @@ const knex = require('../../config/db');
 const getQuestions = async () => {
   try {
     return await knex('questions')
-      .join('game_factory', 'questions.fk_game_id', '=', 'game_factory.id')
+      .join(
+        'game_factory',
+        'questions.fk_game_factory_id',
+        '=',
+        'game_factory.id',
+      )
       .select('questions.question');
   } catch (error) {
     return error.message;
