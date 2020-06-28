a8960957671a8774d7eb5e8e253503a545a7a95e
krithi-kondana <krithi1789@gmail.com>
2020-06-28 10:53:19 +0200

    adding 404 error page

--- src/client/containers/404Page/404Page.component.js
+++ src/client/containers/404Page/404Page.component.js
@@ -1,4 +1,30 @@
 import React from 'react';
 import HeaderTitle from '../../components/Title/Title';
+import ButtonComponent from '../../components/SubmitButton/SubmitButton.js';
+import FooterComponent from '../../components/FooterComponent/FooterComponent.js';
+import ErrorLogo from '../../assets/images/background/flag404.png';
+import '../../App.css';
+import './404Page.styles.css';
 
-export const ErrorPage = () => <HeaderTitle title="This is the 404 Page" />;
+export const ErrorPage = () => {
+  return (
+    <div className="bodycontainer error-page-container">
+      <HeaderTitle title="Error 404" />
+      <div className="bodywrapper">
+        <div className="error-logo-container">
+          <img src={ErrorLogo} alt="Error Logo" />
+          <h3>Oh no!</h3>
+          <p>
+            We are usually a treasure chest, but we could not find what you are
+            looking for.
+          </p>
+        </div>
+        <ButtonComponent
+          ButtonTitle="Try again"
+          onClick={() => history.go(-1)}
+        />
+      </div>
+      <FooterComponent />
+    </div>
+  );
+};
