d7df09d92d009d8606736a6f450cf5f8cf97f152
John <john.yazji@gmail.com>
2020-06-28 14:46:02 +0200

    to deploy on netlify

---
+++ .out/main.8013c6e28d7a9bb9b03e.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{112:function(module,exports,__webpack_require__){__webpack_require__(113),module.exports=__webpack_require__(281)},180:function(module,exports){}},[[112,1,2]]]);
+//# sourceMappingURL=main.8013c6e28d7a9bb9b03e.bundle.js.map
---
+++ .out/precache-manifest.7c5d412dd86c193924f4e8f2335456ca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4c308545444a9f435bb862ee698dd0",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8013c6e28d7a9bb9b03e.bundle.js"
+  },
+  {
+    "url": "runtime~main.8013c6e28d7a9bb9b03e.bundle.js"
+  },
+  {
+    "url": "vendors~main.8013c6e28d7a9bb9b03e.bundle.js"
+  },
+  {
+    "url": "vendors~main.8013c6e28d7a9bb9b03e.bundle.js.LICENSE.txt"
+  }
+]);
