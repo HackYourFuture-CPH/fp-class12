c6430f749eb795073ebdaed159f7c31af852d058
Mekdes <yesmek@yahoo.com>
2020-06-28 12:22:31 +0200

    adding share game page

--- src/client/containers/ShareGamePage/ShareGamePage.component.js
+++ src/client/containers/ShareGamePage/ShareGamePage.component.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
+import './shareGame.css';
 import SubmitButton from '../../components/SubmitButton/SubmitButton';
 import HeaderTitle from '../../components/Title/Title';
 import Footer from '../../components/FooterComponent/FooterComponent';
@@ -7,11 +8,28 @@ import QRcode from '../../components/QRcode/QRcode';
 import { Link } from 'react-router-dom';
 
 const level = 'L';
+
+function QrCode() {
+  const [qrCode, SetQrCode] = useState('');
+  fetch(`/api/qr/`)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result.qr);
+      const gameCode = result.qr;
+
+      SetQrCode(gameCode);
+    });
+
+  return <p className="qrcode">{qrCode}</p>;
+}
+
 export const ShareGamePage = () => (
   <section className="home bodycontainer">
     <HeaderTitle title="Scan QR-Code" />
     <div className="bodywrapper">
-      <p>share this code to the players to share this code</p>
+      <p className="sharegametext">
+        share this code to the players to share this code
+      </p>
       <div className="homelogocontainer">
         <div className="QRcode">
           <QRcode
@@ -23,6 +41,7 @@ export const ShareGamePage = () => (
             includeMargin={false}
           />
         </div>
+        <QrCode />
       </div>
       <Link to="/">
         <SubmitButton ButtonTitle="Back to Game" />
