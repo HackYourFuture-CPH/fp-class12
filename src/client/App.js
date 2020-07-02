6e88cde749c9bd40548090fe50e3b8e902eb8405
nig <nig@beepanalytics.com>
2020-07-02 21:12:40 +0200

    wip

--- src/client/App.js
+++ src/client/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './containers/Home/Home';
 import { ErrorPage } from './containers/404Page/404Page.component';
@@ -10,43 +10,52 @@ import { PickAvatarPage } from './containers/PickAvatarPage/PickAvatarPage.compo
 import { PickGamePage } from './containers/PickGamePage/PickGamePage.component';
 import { ShareGamePage } from './containers/ShareGamePage/ShareGamePage.component';
 import { StartPage } from './containers/StartPage/StartPage.component';
+import { GameContext } from './gameContext';
 
 function App() {
+  const [gameState, setGameState] = useState({
+    user: null,
+    nextQuestion: null,
+    gameInstanceId: null,
+  });
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/join-game-page">
-          <JoinGamePage />
-        </Route>
-        <Route exact path="/create-team-page">
-          <CreateTeamPage />
-        </Route>
-        <Route exact path="/game-page">
-          <GamePage />
-        </Route>
-        <Route exact path="/leaderboard-page">
-          <LeaderboardPage />
-        </Route>
-        <Route exact path="/pick-avatar-page">
-          <PickAvatarPage />
-        </Route>
-        <Route exact path="/pick-game-page">
-          <PickGamePage />
-        </Route>
-        <Route exact path="/share-game-page">
-          <ShareGamePage />
-        </Route>
-        <Route exact path="/start-page">
-          <StartPage />
-        </Route>
-        <Route exact path="/404-page">
-          <ErrorPage />
-        </Route>
-      </Switch>
-    </Router>
+    <GameContext.Provider value={{ gameState, setGameState }}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/join-game-page">
+            <JoinGamePage />
+          </Route>
+          <Route exact path="/create-team-page">
+            <CreateTeamPage />
+          </Route>
+          <Route exact path="/game-page">
+            <GamePage />
+          </Route>
+          <Route exact path="/leaderboard-page">
+            <LeaderboardPage />
+          </Route>
+          <Route exact path="/pick-avatar-page">
+            <PickAvatarPage />
+          </Route>
+          <Route exact path="/pick-game-page">
+            <PickGamePage />
+          </Route>
+          <Route exact path="/share-game-page">
+            <ShareGamePage />
+          </Route>
+          <Route exact path="/start-page">
+            <StartPage />
+          </Route>
+          <Route exact path="/404-page">
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </Router>
+    </GameContext.Provider>
   );
 }
 export default App;
--- src/client/containers/ShareGamePage/ShareGamePage.component.js
+++ src/client/containers/ShareGamePage/ShareGamePage.component.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import HeaderTitle from '../../components/Title/Title';
+import { GameContext } from '../../gameContext';
 
-export const ShareGamePage = () => (
-  <HeaderTitle title="This is the Share Game Page" />
-);
+export const ShareGamePage = () => {
+  const gameState = useContext(GameContext);
+  // show state:
+  console.log(gameState.gameState);
+  // write to state:
+  function setUser(username) {
+    gameState.setGameState((prev) => {
+      return {
+        ...prev,
+        user: username,
+      };
+    });
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={() => setUser('me')}>
+        Set user
+      </button>
+      <HeaderTitle title="This is the Share Game Page" />
+    </div>
+  );
+};
