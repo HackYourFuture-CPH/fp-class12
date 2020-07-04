17e338234a08196b265e6512fef14ddf51b6bd30
amerdahabi <aamerdahabi@gmail.com>
2020-07-04 20:38:03 +0200

    join answer given table

--- src/server/api/controllers/resolve-question.controller.js
+++ src/server/api/controllers/resolve-question.controller.js
@@ -4,26 +4,33 @@ const resolveQuestion = async (id) => {
   try {
     const nextQuestion = await knex('questions')
       .join(
-        'actual_answers',
+        'answers_given',
         'questions.id',
-        '=',
-        'actual_answers.fk_question_id',
+        '>',
+        'answers_given.fk_question_id',
       )
+      .where('answers_given.id', '=', id)
+      .select('questions.question');
+    if (nextQuestion.length === 0) {
+      throw new Error(`Incorrect entry with the id of ${id}`, 404);
+    }
+    const answerResult = await knex('answer_choices')
       .join(
-        'answer_choices',
+        'answers_given',
         'answer_choices.id',
         '=',
-        'actual_answers.fk_answer_id',
+        'answers_given.fk_answer_id',
       )
-      .select('questions.question')
-      .where('answer_choices.answer_correct', '=', '1')
-      .where('actual_answers.id', '=', id)
-      .limit(1);
-
-    if (nextQuestion.length === 0) {
-      throw new Error(`wrong answer`, 404);
+      .select('answer_choices.answer_correct')
+      .where('answers_given.id', '=', id)
+      .where('answer_choices.answer_correct', '=', true);
+    if (answerResult.length === 0) {
+      throw new Error(`Incorrect entry with the id of ${id}`, 404);
     }
-    return nextQuestion[0];
+    return [
+      { nextQuestion: nextQuestion[0] },
+      { answerResult: answerResult[0] },
+    ];
   } catch (error) {
     return error.message;
   }
